96fadfee19d5a9e2a897aea9464907d76bf0344a
unknown <3bmod@e041edu.local>
2026-03-30 20:30:37 -0300

    introduzindo export default e routes in app.jsx

--- atividades/ex3003/src/App.jsx
+++ atividades/ex3003/src/App.jsx
@@ -1,3 +1,16 @@
+import { Routes } from 'react-router-dom'
 import Home from './pages/home'
 
 // Dentro de <Routes>, adicione como PRIMEIRA rota:
+export default function App(){
+  return(
+<Routes>
+<Route path='/'            element={<home />}         />
+<Route path='/trapezio'    element={<Trapezio />}     />
+<Route path='/imc'         element={<IMC />}          />
+// ... demais rotas
+</Routes>
+  )
+}
+
+
