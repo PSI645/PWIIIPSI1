44ab2db2ba55f4ed5f3821eddf0ef9a6954512f1
unknown <3bmod@e041edu.local>
2026-04-07 19:57:06 -0300

    modificação da guia app adição de route media escolar

--- atividades/aula0604/projetoaula0604/src/App.jsx
+++ atividades/aula0604/projetoaula0604/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Inicio from './pages/Inicio'
+import MediaEscolar from './pages/MediaEscolar'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path="media" element={<MediaEscolar />} />
         </Route>
       </Routes>
     </BrowserRouter>
